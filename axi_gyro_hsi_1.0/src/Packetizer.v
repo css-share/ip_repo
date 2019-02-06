7b2902143ad85271f5cc72e3a2d8b7058e484284
tawender <tawender@customsiliconsolutions.com>
2019-02-05 19:09:47 -0800

    initial commit

---
+++ axi_gyro_hsi_1.0/src/Packetizer.v
@@ -0,0 +1,187 @@
+
+// ----------------------------------------------------------------
+module delay_line_8x32bits (  
+  input  clock,           // input for clock to all flops in the shift register
+  input  reset_n,         // input to reset the register to a default value
+  input  [31:0] data_in,  // input for data to the first flop in the shift register
+  output [31:0] data_out  // output to read out the current value of all register bits
+);
+
+  wire [31:0] s0;
+  wire [31:0] s1;
+  wire [31:0] s2;
+  wire [31:0] s3;
+  wire [31:0] s4;
+  wire [31:0] s5;
+  wire [31:0] s6;
+  wire [31:0] s7;
+
+  register_32bits Reg0(.clock(clock),.reset_n(reset_n),.data_in(data_in),.data_out(s0));
+  register_32bits Reg1(.clock(clock),.reset_n(reset_n),     .data_in(s0),.data_out(s1));
+  register_32bits Reg2(.clock(clock),.reset_n(reset_n),     .data_in(s1),.data_out(s2));
+  register_32bits Reg3(.clock(clock),.reset_n(reset_n),     .data_in(s2),.data_out(s3));
+  register_32bits Reg4(.clock(clock),.reset_n(reset_n),     .data_in(s3),.data_out(s4));
+  register_32bits Reg5(.clock(clock),.reset_n(reset_n),     .data_in(s4),.data_out(s5));
+  register_32bits Reg6(.clock(clock),.reset_n(reset_n),     .data_in(s5),.data_out(s6));
+  register_32bits Reg7(.clock(clock),.reset_n(reset_n),     .data_in(s6),.data_out(s7));
+
+  assign data_out = s7;
+
+endmodule
+
+module Packetizer_fsm (
+  input clock, 
+  input reset_n,
+  input full,
+  input last,
+  output reg inj,
+  output reg shift_sel,
+  output reg valid,
+  output reg rst_sr
+);
+ 
+  parameter
+    S0 = 2'b00,
+    S1 = 2'b01,
+    S2 = 2'b10,
+    S3 = 2'b11;
+
+  reg  [1:0] state;
+  reg  [1:0] next_state;
+
+//----------Seq Logic-----------------------------
+ always @ (posedge (clock) or negedge(reset_n))
+ begin
+    if (reset_n == 1'b0)
+     begin
+      state <=  S0;
+      end 
+    else begin
+      state <= next_state;
+    end
+ end
+
+// Combinatorial Logic----------------------------- 
+  always @(state or full or last)
+    begin
+      next_state = S0;
+      case(state)
+        S0: 
+          begin
+	    inj     <= 1'b1;
+	    shift_sel     <= 1'b0;
+	    rst_sr  <= 1'b1;
+	valid <= 1'b0;
+	    if(full == 1'b0)
+	      begin
+                next_state = S0; 
+	      end
+	    else
+	      begin
+                next_state = S1; 
+	      end
+	  end
+        S1: 
+	  begin
+	    inj     <= 1'b0;
+	    shift_sel <= 1'b1;
+	    rst_sr  <= 1'b1;
+valid <= 1'b0;
+            next_state = S2; 
+          end
+	S2:
+	  begin
+	    inj     <= 1'b0;
+	    shift_sel   <= 1'b1;
+	    rst_sr  <= 1'b1;
+valid <= 1'b1;
+	    if(last == 1'b0)
+	      begin
+                next_state = S2; 
+	      end
+	    else
+	      begin
+                next_state = S3; 
+	      end
+          end
+        S3:
+	  begin
+	    inj     <= 1'b0;
+	    shift_sel    <= 1'b0;
+	    rst_sr  <= 1'b0;
+valid <= 1'b0;
+	    next_state = S0;
+	  end
+	default:
+	  begin
+	    inj     <= 1'b0;
+	    shift_sel     <= 1'b0;
+valid <= 1'b0;
+	    rst_sr  <= 1'b1;
+	    next_state   = S0;
+	  end     
+       endcase
+  end
+
+endmodule
+
+// ----------------------------------------------------------------
+
+module Packetizer (
+  input clock,
+  input reset_n,
+  input [31:0] data_in,
+  input valid,
+  output TCLK,
+  output [31:0] TDATA,
+  output TVALID,
+  output TLAST
+);
+
+  wire [31:0] s8;
+  wire [31:0] s9;
+  wire [8:0] ctl_out;
+
+  // wire clock_sel;
+  // wire sync_sel;
+  wire reset_sr;
+
+  wire inj_bit;
+  wire inj_bit_sync;
+  wire last;
+  wire full;
+  wire shift_sel;
+  wire shift_sel_sync;
+  wire valid_fsm;
+
+  Packetizer_fsm FSM (
+    .clock(clock),
+    .reset_n(reset_n),
+    .full(full),
+    .last(last),
+    .inj(inj_bit),
+    .shift_sel(shift_sel),
+    .valid(valid_fsm),
+    .rst_sr(reset_sr)
+  );
+
+  delay_line_8x32bits delayLine (.clock(shift),.reset_n(reset_n),.data_in(data_in),.data_out(s8));
+  register_32bits           Reg9(.clock(shift),.reset_n(reset_n),     .data_in(s8),.data_out(s9));
+  shift_reg_9bits            SR9(.clock(shift),.reset_n(reset_n & reset_sr),.en(1'b1),.d(inj_bit_sync),.out(ctl_out));
+
+  mux_2x1_1bit	       shiftMux(.in0(valid),.in1(clock),.sel(shift_sel_sync),.mux_out(shift));
+
+  dff FF0(.clk(~clock), .rst_n(reset_n), .D(inj_bit), .Q(inj_bit_sync)); 
+  dff FF1(.clk(~clock), .rst_n(reset_n), .D(shift_sel & ~last), .Q(shift_sel_sync)); 
+
+  // select_synchronizer syncMuxSel(.clock(clock),.reset_n(reset_n),.last(last), .sel(clock_sel),.sync_sel(sync_sel));
+
+  assign full   = ctl_out[7];
+  assign last   = (ctl_out[8] & ~ctl_out[7]);
+
+  assign TCLK   = clock;
+  assign TDATA  = s9;
+  assign TLAST  = last;
+  assign TVALID = valid_fsm;
+
+ endmodule
---
+++ axi_gyro_hsi_1.0/src/StreamPipeline.v
@@ -0,0 +1,191 @@
+// -----------------------------------------------------------------------
+// ----------------------------------------------------------------
+module upCounter16Bits (
+    input  wire clock,
+    input  wire reset_n,
+    input  wire enable,
+    output wire [15:0] count
+  );
+ 
+  reg  [15:0] r_reg;
+  wire [15:0] r_next;
+  
+  always @(posedge clock, negedge reset_n)
+    begin
+      if (reset_n == 1'b0)
+        r_reg <= 16'h0000;
+      else
+        if(enable == 1'b0)
+          r_reg <= r_reg;
+        else
+          r_reg <= r_next;
+    end
+  
+    assign count  = r_reg;
+    assign r_next = r_reg + 1;
+  
+endmodule
+
+// ----------------------------------------------------------------
+module StreamDebugger(
+  input clock,
+  input reset_n,
+  input clear,
+  input valid_token,
+  input tclk,
+  input tvalid,
+  input tlast,
+  output [31:0] word0,
+  output [31:0] word1
+);
+
+  // -----------------------------------------------------
+
+  wire [15:0] data0_int;
+  wire [15:0] data1_int;
+  wire [15:0] data2_int;
+
+  // -----------------------------------------------------
+
+  upCounter16Bits CNTR0(.clock(valid_token),.reset_n(reset_n & ~clear),.enable(1'b1),.count(data0_int));
+  upCounter16Bits CNTR1(.clock(tlast),.reset_n(reset_n & ~clear),.enable(1'b1),.count(data1_int));
+  upCounter16Bits CNTR2(.clock(tvalid & (~tclk)),.reset_n(reset_n & ~clear),.enable(1'b1),.count(data2_int));
+
+  assign word0 = { 16'h0000, data0_int};
+  assign word1 = { data2_int, data1_int};
+
+endmodule
+
+// -----------------------------------------------------------------------
+
+module StreamPipeline(
+    input clock,
+    input reset_n,
+    input startSamples,
+    input stopSamples,
+    input holdSamples,
+    input clearDebugRegisters,
+    input [15:0] numSamples,
+    input [1:0] mode,
+    output doneSamples,
+    input [1:0] channelSelect,
+    input HSCK_POL,
+    input [1:0] HSDATA,
+    output MCK,
+    output [1:0] HSCK,
+    output [31:0] dbg_word0,
+    output [31:0] dbg_word1,
+    // output HSCK_dbg,
+    // output HSDATA_dbg,
+    // output validToken,
+    output TCLK,
+    output [31:0] TDATA,
+    output TVALID,
+    output TLAST
+  );
+
+  // ---------------------------------------------------------------------
+
+  wire MCK_int;
+  wire tclk_int;
+  wire tvalid_int;
+  wire tlast_int;
+  wire HSCK_int;
+  wire HSDATA_int;
+  wire HSDATA_gen;
+
+  wire [31:0] data_out;
+
+  wire [31:0] dbg_word0_int;
+  wire [31:0] dbg_word1_int;
+
+  wire valid_out;
+  wire clock_int;
+
+  wire nc3, nc2;
+
+  // ---------------------------------------------------------------------
+  // TODO: fix all the output HSK and its genberation and control flow.
+  // ---------------------------------------------------------------------
+
+  demux_1_to_4_pol HSSCKdemux(
+    .mux_in(HSCK_int),
+    .pol(HSCK_POL),
+    .sel(channelSelect),
+    .mux_out({nc3, nc2, HSCK[1:0]})
+  );
+
+  mux_4_to_1 HSDATAmux(
+    .mux_out(HSDATA_int),
+    .sel(channelSelect),
+    .mux_in({HSDATA_gen, 1'b0, HSDATA[1:0]})
+  );
+
+clock_divider_by_2 CLK_DIV2(
+  .clk_in(clock),
+  .rst_n(reset_n),
+  .clk_out(clock_int)
+);
+
+  StreamGenerator GYRO_StreamGenerator(
+    .clock(clock_int),
+    .reset_n(reset_n),
+    .start(startSamples),
+    .stop(stopSamples),
+    .hold(holdSamples),
+    .mode(mode),
+    .HSCK_POL(HSCK_POL),
+    .channel(channelSelect),
+    .numberSamples(numSamples),
+    .done(doneSamples),
+    .MCK(MCK_int),
+    .HSCK(HSCK_int),
+    .HSDATA(HSDATA_gen)
+  );
+
+ Tokenizer GYRO_Tokenizer(
+  .clock(clock_int),
+  .reset_n(reset_n),
+  .HSCK_POL(HSCK_POL),
+  .HSCK(HSCK_int),
+  .HSDATA(HSDATA_int),
+  .data_out(data_out),
+  .valid_out(valid_out)
+);
+
+ Packetizer GYRO_Packetizer(
+    .clock(clock_int),
+    .reset_n(reset_n),
+    .data_in(data_out),
+    .valid(valid_out),
+    .TCLK(tclk_int),
+    .TDATA(TDATA),
+    .TVALID(tvalid_int),
+    .TLAST(tlast_int)
+  );
+
+ StreamDebugger GYRO_Debugger(
+   .clock(clock_int),
+   .reset_n(reset_n),
+   .clear(clearDebugRegisters),
+   .valid_token(valid_out),
+   .tclk(tclk_int),
+   .tvalid(tvalid_int),
+   .tlast(tlast_int),
+   .word0(dbg_word0_int),
+   .word1(dbg_word1_int)
+ );
+
+  // assign validToken = valid_out;
+  assign MCK    = clock_int;
+  assign TCLK   = tclk_int;
+  assign TVALID = tvalid_int;
+  assign TLAST  = tlast_int;
+
+  assign dbg_word0 = dbg_word0_int;
+  assign dbg_word1 = dbg_word1_int;
+
+  // assign HSCK_dbg = HSCK_int;
+  // assign HSDATA_dbg = HSDATA_int;
+
+endmodule
